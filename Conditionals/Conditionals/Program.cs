f752ffb4f754a4e6c6afa8cf718d7647ef45906c
Adrian <Zilibdev@gmail.com>
2020-01-24 13:28:18 +0100

    Conditions and loops.

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else if(a + b <= 10)
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else if (a + b <= 10)
+            {
+                Console.WriteLine("The answer is not greater than 10\n");
+            }
+            // Counters
+            // From 1 to 10
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.WriteLine($"Hello World! The counter is { counter++ }");
+            }
+            Console.WriteLine("\n\n");
+
+            counter = 1;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is { counter++ }");
+            } while (counter <= 10);
+
+            Console.WriteLine("\n\n");
+
+            for(counter = 1; counter <= 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is { counter }");
+            }
+            // Show even numbers in range from 1 to 20
+            for(int i = 1; i<=20;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Hello, i am going to study .Net, here you will able to check every projects which i am doing.
 Everything what i will do here is got from https://dotnet.microsoft.com/learn/csharp
 ## Projects list
-* **Hello World:** simple string operations. StartsWith / EndsWith / Contains / Lenghts - Simple C# function
-* **Numbers:** First working with types in C#, and constants. Checking how does program work when value of the variable is bigger than can be
+* **Hello World:** simple string operations. StartsWith / EndsWith / Contains / Lenghts - Simple C# function.
+* **Numbers:** First working with types in C#, and constants. Checking how does program work when value of the variable is bigger than can be.
+* **Conditions and loops:** Nothing to add, like in every other programming langues.
