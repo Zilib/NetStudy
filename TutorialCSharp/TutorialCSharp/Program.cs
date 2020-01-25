19086b24383f0902d3953eb0e9bae3eae2b3b796
Adrian <Zilibdev@gmail.com>
2020-01-25 21:51:48 +0100

    Interfaces, working with docs.

--- TutorialCSharp/TutorialCSharp/Program.cs
+++ TutorialCSharp/TutorialCSharp/Program.cs
@@ -2,6 +2,59 @@
 
 namespace TutorialCSharp
 {
+    interface IEnglishDimensions
+    {
+        float Length();
+        float Width();
+    }
+    interface IMetricDimensions
+    {
+        float Length();
+        float Width();
+    }
+    class Box : IEnglishDimensions, IMetricDimensions
+    {
+        float lengthInches { get; }
+        float widthInches { get; }
+        public float Length() => lengthInches * 2.54f;
+        public float Width() => widthInches * 2.54f;
+
+        public Box(float lengthInches, float widthInches)
+        {
+            this.lengthInches = lengthInches;
+            this.widthInches = widthInches;
+        }
+
+
+        float IEnglishDimensions.Length() => lengthInches;
+        float IEnglishDimensions.Width() => widthInches;
+
+        float IMetricDimensions.Length() => lengthInches * 2.54f;
+        float IMetricDimensions.Width() => widthInches * 2.54f;
+
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(30.0f, 20.0f);
+
+            IEnglishDimensions eDimensions = box1;
+            IMetricDimensions mDimensions = box1;
+
+            Console.WriteLine($"{"Length(in):", -10} {eDimensions.Length(), -5}");
+            Console.WriteLine($"{"Width (in):", -10} {eDimensions.Width(), -5}\n");
+
+            Console.WriteLine($"{"Length(in):",-10} {mDimensions.Length(), -5}");
+            Console.WriteLine($"{"Width (in):",-10} {mDimensions.Width(), -5}\n");
+
+            Console.WriteLine($"{"Length(in):",-10} {box1.Length(),-5}");
+            Console.WriteLine($"{"Width (in):",-10} {box1.Width(),-5}");
+        }
+    }
+
+}
+
+/*
+namespace TutorialCSharp
+{
     interface IDimensions
     {
         float getLength();
@@ -33,3 +86,4 @@ namespace TutorialCSharp
         }
     }
 }
+*/
