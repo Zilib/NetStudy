2cf662928f2c9011437358fd39b82eb53763bd33
Adrian <Zilibdev@gmail.com>
2020-02-02 16:43:48 +0100

    Tutorial part 2, file explorer

---
+++ WpfTreeView/Directory/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// A base view model that fires Property changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
---
+++ WpfTreeView/Directory/ViewModels/DirectoryStructureViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// The view model for applications main Directory view
+    /// </summary>
+    public class DirectoryStructureViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// A list of all directories on the machine
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Items { get; set; }
+
+        #endregion
+
+        #region Constructor
+        
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DirectoryStructureViewModel()
+        {
+            // get the logical drives
+            var children = DirectoryStructure.GetLogicalDrives();
+
+            // Create the view models from the data
+            this.Items = new ObservableCollection<DirectoryItemViewModel>(
+                children.Select(drive => new DirectoryItemViewModel(drive.FullPath, DirectoryItemType.Drive)));
+        }
+
+        #endregion
+    }
+}
